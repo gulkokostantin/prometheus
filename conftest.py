e0c8c692b314183b5badbe7016242a02e7599050
gulkokostantin <59530217+gulkokostantin@users.noreply.github.com>
2024-05-17 17:15:41 +0200

    Conftest + pytest ini

---
+++ conftest.py
@@ -0,0 +1,28 @@
+import pytest
+
+
+class User:
+
+    def __init__(self) -> None:
+        self.name = None
+        self.second_name = None
+
+    def create(self):
+        self.name = "Kostiantyn"
+        self.second_name = "Gulko"
+
+    def remove(self):
+        self.name = ""
+        self.second_name = ""
+
+
+@pytest.fixture
+def user():
+    # Створити об'єкт класу User
+    user = User()
+    # Викликати метод об'єкту create
+    user.create()
+    # Повернути об'єкт після виклику методу об'єкту create в тести
+    yield user
+    # Після виконання тесту викликати метод об'єкту remove
+    user.remove()
