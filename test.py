cd26d7fbb71a4ec67b788971fd2a5b94ba03444b
gulkokostantin <59530217+gulkokostantin@users.noreply.github.com>
2024-05-17 10:20:06 +0200

    Project test1

--- test.py
+++ test.py
@@ -1,2 +1,42 @@
-#test
-#git pull test
+import os
+
+
+def create_structure():
+    # Перевірка наявності папок перед їх створенням
+    folders_to_create = [
+        "config",
+        "modules/common",
+        "modules/ui/page_objects",
+        "modules/api/clients",
+        "tests/ui",
+        "tests/api"
+    ]
+
+    for folder in folders_to_create:
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+        else:
+            print(f"Папка '{folder}' вже існує.")
+
+    # Створення файлів
+    files_to_create = [
+        "config/config.py",
+        "modules/common/__init__.py",
+        "modules/ui/__init__.py",
+        "modules/ui/page_objects/__init__.py",
+        "modules/api/__init__.py",
+        "modules/api/clients/__init__.py",
+        "tests/ui/test_ui.py",
+        "tests/api/test_api.py"
+    ]
+
+    for file in files_to_create:
+        if not os.path.exists(file):
+            with open(file, "w") as f:
+                pass
+        else:
+            print(f"Файл '{file}' вже існує.")
+
+
+if __name__ == "__main__":
+    create_structure()
